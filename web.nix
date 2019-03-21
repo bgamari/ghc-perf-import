fba18d98d1b4cf4b19076fb7a69863b3f54c0e7b
Ben Gamari <ben@smart-cactus.org>
2019-03-21 12:43:27 -0400

    Fix it

--- web.nix
+++ web.nix
@@ -7,7 +7,11 @@ in
   stdenv.mkDerivation {
     name = "ghc-perf-web";
     src = nodePkg.package;
+    nativeBuildInputs = [ nodePkg.shell nodePackages.browserify ];
     sourceRoot = "${nodePkg.package.name}/lib/node_modules/ghc-perf-import";
+    buildPhase = ''
+      browserify ghc_perf.js -o bundle.js
+    '';
     installPhase = ''
       mkdir $out
       make install DESTDIR=$out
